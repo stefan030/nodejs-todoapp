6df13b73a4009cb0ac36ae3d1507726c11b264e6
stefan <stefanbogdanovic030@gmail.com>
2019-01-04 13:33:53 +0100

    POST and DELETE handling requests

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,15 +1,35 @@
+var bodyParser = require('body-parser');
+
+var data = [
+    {
+        item: 'get milk'
+    },
+    {
+        item: 'walk dog'
+    },
+    {
+        item: 'code'
+    }
+];
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
 module.exports = (app) => {
 
     app.get('/todo', (req, res) => {
-        res.render('todo');
+        res.render('todo', {todos: data});
     });
 
-    app.post('/todo', (req, res) => {
-
+    app.post('/todo', urlencodedParser, (req, res) => {
+        data.push(req.body); // push received data to the array
+        res.json({todos: data}); // send updated data array as json back to front end
     });
 
-    app.delete('/todo', (req, res) => {
-
+    app.delete('/todo/:item', (req, res) => {
+        data = data.filter((todo) => { // Goal is to return false so we can filter out selected task from todo (data array)
+           return todo.item.replace(/ /g, '-') !== req.params.item;
+        });
+        res.json({data});
     });
 
 };
